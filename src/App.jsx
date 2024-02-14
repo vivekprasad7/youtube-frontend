087ed951c55ee8c835672e4d70d63557ea7b7fc0
Vivek Prasad <flashtech007@gmail.com>
2024-02-14 11:44:27 +0530

    fix - tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import "./App.css"
+
 function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-red-900 text-3xl font-bold border-black'>
       <h1>React</h1>
       </div>
     </>
