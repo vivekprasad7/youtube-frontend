fd308130980b803f58334cd427a454a794a06058
Vivek Prasad <flashtech007@gmail.com>
2024-02-20 21:20:19 +0530

    fix - mixed content error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,6 @@ export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/vid
 // Authorization: Bearer [YOUR_ACCESS_TOKEN]
 // Accept: application/json
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const YOUTUBE_SEARCH_VIDEOS = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=" + GOOGLE_API_KEY + "&q="
